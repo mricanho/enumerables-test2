c746edf26fec56a2afaed1402f82d104fd72c243
Miguel Ricaño <mricanho@gmail.com>
2020-12-02 14:39:10 -0600

    Add my_select

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ module Enumerable
       x += 1
     end
   end
+
   def my_each_with_index
     i = 0
     while i < length
@@ -14,9 +15,16 @@ module Enumerable
     end
     self
   end
+  def my_select
+    result = []
+    my_each { |x| result << x if yield(x) }
+    result
+  end
 end
 
 puts '1.--------my_each--------'
 %w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
 puts '2.--------my_each_with_index--------'
 %w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
+puts '3.--------my_select--------'
+puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
