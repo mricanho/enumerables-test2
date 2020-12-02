e229635985432bed631a2bb06cc4b3c468a0e60f
Miguel Ricaño <mricanho@gmail.com>
2020-12-02 14:36:39 -0600

    Add my_each_with_index

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   def my_each
     x = 0
@@ -7,4 +6,17 @@ module Enumerable
       x += 1
     end
   end
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
+
+puts '1.--------my_each--------'
+%w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
+puts '2.--------my_each_with_index--------'
+%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
