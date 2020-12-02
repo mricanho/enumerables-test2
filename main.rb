0e74c8308a36a8e7c0a9fa0918ed00eb605a37af
Miguel Ricaño <mricanho@gmail.com>
2020-12-02 14:41:05 -0600

    Add my_all

--- main.rb
+++ main.rb
@@ -15,10 +15,21 @@ module Enumerable
     end
     self
   end
+
   def my_select
     result = []
     my_each { |x| result << x if yield(x) }
     result
+  end
+
+  def my_all?(*)
+    unless block_given?
+      my_each { |i| return false if i.nil? || i == false }
+      return true
+    end
+    count = 0
+    my_each { |i| count += 1 if yield(i) }
+    count == length
   end
 end
 
@@ -27,4 +38,10 @@ puts '1.--------my_each--------'
 puts '2.--------my_each_with_index--------'
 %w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
 puts '3.--------my_select--------'
-puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
+puts(%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
+puts '4.--------my_all--------'
+puts(%w[ant bear cat].my_all? { |word| word.length >= 3 }) #=> true
+puts(%w[ant bear cat].my_all? { |word| word.length >= 4 }) #=> false
+puts %w[ant bear cat].my_all?(/t/) #=> false
+puts [1, 2i, 3.14].my_all?(Numeric) #=> true
+puts [].my_all? #=> true
